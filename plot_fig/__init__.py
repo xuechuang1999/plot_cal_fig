367e2da4c6b0ac97ad0d1c1516bc967f6a322f22
xuechuang1999 <2321052461@qq.com>
2025-05-16 23:59:29 +0800

    touch files...

---
+++ plot_fig/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+__name__ = "plot_fig"
+__version__ = "0.0.0"
+__author__ = "chxue"
+__email__ = "ch.xue99@gmail.com"
+__description__ = 'Plot the PDOS and band structure of DFT calculation results'
+__license__ = 'GPLv3'
+__url__ = '../README.md'
+__epilog__ = 'The current version (v' + __version__ + ') may contain bugs. To get the latest version, please run:\
+\n\n\t$ pip3 install --upgrade plot_fig\n\nWe also recommend you to see the documentation at:\
+\n\n\t' + __url__ + '\n\n\
+You are also welcome to contact me at ' + __email__ + ' for any questions, feedbacks or comments.'
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+import re
+import setuptools
+
+def parse_init_file():
+    init_path = os.path.join(os.path.dirname(__file__), 'plot_fig', '__init__.py')
+    variables = {}
+    pattern = re.compile(r'^__(\w+)__\s*=\s*[\'"]([^\'"]+)[\'"]')
+    with open(init_path, 'r', encoding='utf-8') as f:
+        for line in f:
+            match = pattern.match(line.strip())
+            if match:
+                key, value = match.groups()
+                variables[key] = value
+    return variables
+
+metadata = parse_init_file()
+
+with open("README.md", "r", encoding="utf-8") as fhand:
+    long_description = fhand.read()
+
+setuptools.setup(
+    name=metadata["name"],
+    version=metadata["version"],
+    author=metadata["author"],
+    author_email=metadata["email"],
+    description=(metadata["description"]),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=metadata["url"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["numpy", "scipy", "spglib", "numba", "matplotlib", "tqdm"],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.9",
+    entry_points={
+        "console_scripts": [
+            # "test = test:main",
+        ]
+    }
+)
